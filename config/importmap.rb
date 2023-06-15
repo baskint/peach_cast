f2aa1c8d466596f8510f80d6e9edba9abd6f3692
Baskin Tapkan <baskint@gmail.com>
2023-06-14 22:50:50 -0500

    importmap pin lodash

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "lodash", to: "https://ga.jspm.io/npm:lodash@4.17.21/lodash.js"
